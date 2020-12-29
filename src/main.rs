e12be107bfed757f7157de9f8969855fab63922f
Vegard Sandengen <vegardsandengen@gmail.com>
2020-12-29 21:35:26 +0100

    feat: initial working state

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,19 @@
-use chrono::prelude::*;
-use chrono::offset::Utc;
-use structopt::StructOpt;
+//! An interest calculator with with daily status report.
+//!
+//! This interest calculator supports the following features:
+//! * Add an arbitrary number of extra downpayments
+//! * Get a daily/monthly/yearly state report over the coarse of the loan.
 
-use num_traits::cast::FromPrimitive;
+mod cli;
+mod plot;
+mod process;
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "interest-calculator")]
-struct Opt {
-    /// Total sum of the loan.
-    #[structopt(default_value = "4350000")]
-    loan: i64,
-    /// Number of terms to pay back the entire loan.
-    /// Incompatible with the `years` option.
-    #[structopt(short, long)]
-    terms: Option<i32>,
-    /// Number of years to pay back the entire loan.
-    /// Spread out across `terms_per_year` to determine the number of terms to cover the
-    /// entire loan.
-    /// Incompatible with the `terms` option.
-    #[structopt(short, long, conflicts_with("terms"))]
-    years: Option<i32>,
-    /// Number of terms per year.
-    #[structopt(long, default_value = "12")]
-    terms_per_year: i32,
-
-    /// Interest over an entire year.
-    #[structopt(short, long, default_value = "1.25")]
-    interest: f32,
-    /// Incurring cost for each term payment.
-    #[structopt(short, long, default_value = "45")]
-    fee: i32,
-}
+pub use plot::*;
+pub use process::*;
 
 fn main() {
-    let opt = Opt::from_args();
+    #[cfg(wasm)]
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-    // Sanify how many terms_per_year we can do
-    // I think its safe to assume that only a few combinations make sense
-    const ALLOWED_TERMS_PER_YEAR: [i32; 5] = [1, 2, 4, 6, 12];
-    if !ALLOWED_TERMS_PER_YEAR.contains(&opt.terms_per_year) {
-        eprintln!("error: The argument '--terms-per-year <num>' must be one of {:?}", ALLOWED_TERMS_PER_YEAR);
-        return;
-    }
-
-    let terms = match (opt.terms, opt.years) {
-        (Some(t), None) => t,
-        (None, Some(y)) => y * opt.terms_per_year,
-        (None, None) => 30 * opt.terms_per_year,
-        (Some(_), Some(_)) => unreachable!(),
-    };
-
-    println!("{:?}", opt);
-    println!("Number of terms: {}", terms);
-
-    // Get the next month and the year that belongs to
-    let current_time = Utc::now();
-    let month = Month::from_u32(current_time.month()).unwrap().succ();
-    let year = if month.number_from_month() == 1 {
-        current_time.year() + 1
-    } else {
-        current_time.year()
-    };
-
-    println!("Starting calculation from {} {}", month.name(), year);
+    process();
 }
---
+++ src/plot.rs
@@ -0,0 +1,85 @@
+use crate::{MonthlyResult, TotalResult};
+
+use chrono::Month;
+use num_traits::FromPrimitive;
+use plotters::prelude::*;
+
+pub fn create_plot(
+    monthly: Vec<MonthlyResult>,
+    total: TotalResult,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let root = SVGBackend::new("test.svg", (1240, 1028)).into_drawing_area();
+    root.fill(&WHITE)?;
+    let root = root.margin(10, 20, 50, 150);
+
+    // After this point, we should be able to draw construct a chart context
+    let mut chart = ChartBuilder::on(&root)
+        // Set the caption of the chart
+        .caption("Loan Payment Progress", ("sans-serif", 40).into_font())
+        // Set the size of the label region
+        .x_label_area_size(20)
+        .y_label_area_size(40)
+        // Set the ranges for the X and Y axis
+        // X should be the number of total terms
+        // Y should be the total payment
+        .build_cartesian_2d(0..total.planned_terms, 0f64..total.total_cost)?;
+
+    // Then we can draw a mesh
+    chart
+        .configure_mesh()
+        // We can customize the maximum number of labels allowed for each axis
+        .x_labels(20)
+        .y_labels(10)
+        // We can also change the format of the label text
+        .y_label_formatter(&|x| format!("{:.3}", x))
+        .draw()?;
+
+    // Construct the (X, Y) series data for monthly current_loan
+    let remaining: Vec<(i32, f64)> = monthly
+        .iter()
+        .enumerate()
+        .map(|(term, m)| (term as i32, m.current_loan))
+        .collect();
+
+    // Construct the (X, Y) series of total cost
+    let mut sum: f64 = 0.0;
+    let cost: Vec<(i32, f64)> = monthly
+        .iter()
+        .enumerate()
+        .map(|(term, x)| {
+            sum += x.payed_back;
+            (term as i32, sum.clone())
+        })
+        .collect();
+
+    // Draw a line between each point
+    chart.draw_series(LineSeries::new(remaining.clone(), &RED))?;
+
+    chart.draw_series(LineSeries::new(cost.clone(), &BLACK))?;
+
+    // Draw the text of the last element
+    let last_point: (i32, f64) = *cost.last().unwrap();
+    chart.draw_series(PointSeries::of_element(
+        vec![*remaining.first().unwrap(), last_point],
+        5,
+        &BLACK,
+        &|c, s, st| {
+            return EmptyElement::at(c)    // We want to construct a composed element on-the-fly
+            + Circle::new((0,0),s,st.filled()) // At this point, the new pixel coordinate is established
+            + Text::new(format!("{:.0?}", c.1), (10, 0), ("sans-serif", 30).into_font());
+        },
+    ))?;
+
+    // Month/Year of last payment
+    let last = monthly.last().unwrap();
+    let text = format!(
+        "{} {}",
+        Month::from_u32(last.month).unwrap().name(),
+        last.year
+    );
+    let cords = chart.backend_coord(remaining.last().unwrap());
+    let style = TextStyle::from(("sans-serif", 30).into_font()).color(&RED);
+    root.draw_text(&text, &style, (cords.0 - 60, cords.1 - 40))?;
+
+    Ok(())
+}
